d591b1d23a6eb76b40ff31251eb2995e74514073
Josh <118862870+Catch2two@users.noreply.github.com>
2023-04-10 10:53:59 -0400

    Creating a addCard Function to project to HTML

--- Library Basic/main.js
+++ Library Basic/main.js
@@ -1,28 +1,4 @@
-// Modal Setup (UI)
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close")
-
-const addBook = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-}
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-
-openModalBtn.addEventListener("click", addBook);
-closeModalBtn.addEventListener("click", closeModal)
-
-// Animations
-
-
-// Add to Library Structure
-
+// Prototypes
 class Book {
   constructor(
     title = 'No title',
@@ -36,57 +12,64 @@ class Book {
     this.isRead = isRead
   }
 }
-
-// Card Actions
-
 class Library {
   constructor() {
-    this.book = []
+    this.books = []
   }
 
-  addBook(newBook) {
-    if (!this.isInLibrary(newBook)) {
-      this.books.push(newBook)
-    }
+  addLibrary(newBook) {
+    this.books.push(newBook)
   }
+}
+// CREATE BOOK CARD
 
-  deleteBook(title) {
-    this.books = this.books.filter
-    ((book) => book.title !== title)
-  }
 
-  getBook(title) {
-    return this.books.find
-    ((book) => book.title === newBook.title)
-  }
-/* The some() method tests whether at least one element in the array passes 
-the test implemented by the provided function. It returns true if, in 
-the array, it finds an element for which the provided function returns true;
- otherwise it returns false. It doesn't modify the array. */
- 
-  isInLibrary(newBook) {
-    return this.books.some
-    ((book.title === newBook.title))
+
+// Modal Setup (UI)
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close")
+const closeModalOverlay = document.querySelector(".overlay")
+const submitBtn = document.querySelector('.btn submit')
+
+
+const plusBookBtn = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+openModalBtn.addEventListener("click", plusBookBtn);
+closeModalBtn.addEventListener("click", closeModal);
+closeModalOverlay.addEventListener("click", closeModal);
+closeModalOverlay.addEventListener("click", closeModal);
+
+//Add Book Card to library
+
+const createBookInput = () => {
+  resetBookInput()
+  for (let book of Library.books) {
+    addBookCard(book)
   }
 }
 
-const library = new Library
-
-// Add Book
+// Adding the HTML (Visual) of the New Book to Library
 
 const addBookCard = (book) => {
-  const bookCard = document.createElement('div');
-  const bookAuthor = document.createElement('p');
+  const bookCard = document.createElement('div')
+  const title = document.createElement('p')
+  const author =  document.createElement('p')
+  const pages = document.createElement('p')
 
+  bookCard.classList.add('')
 }
-// Add Example:
-//1. Create a New DIV Element
-let bookCard = document.createElement('div');
-bookCard.classList.add('cards');
-//2. add text content to the DIV ELement
-let titleSubmit = document.createTextNode('This is a Book Card');
-//3. add content to the DIV Element
-bookCard.appendChild(titleSubmit);
-//4. Add the Element to Webpage
-let containerDiv = document.querySelector('.container');
-containerDiv.appendChild(bookCard);
+
+// Animations
+function animateInput() {
+  document.getElementById('btn btn-open').className = 'classname';
+}
