4116e7f256d4ab1666758c0d36729c66d3421a47
Josh <118862870+Catch2two@users.noreply.github.com>
2023-04-10 15:44:41 -0400

    Semi Restarted the JS.

--- Library Basic/main.js
+++ Library Basic/main.js
@@ -1,120 +1,66 @@
-// Prototypes
-class Book {
-  constructor(
-    title = 'No title',
-    author = 'No Author',
-    pages = 0,
-    isRead =  false
-  ) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
-  }
-}
-
-// Storage (The Library that stores the books)
-class Library {
-  constructor() {
-    this.books = []  // Create Array of Added Books
-  }
-
-  addLibrary(newBook) {
-    if (!this.isInLibrary(newBook)) {
-      this.books.push(newBook)
-    }
-  }
-
-  removeBook(title) {
-    this.books = this.books.filter((book) => book.title !== title)
-  }
-}
-
-const library = new Library()
-
-
-
-
 // Modal Setup (UI)
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const openModalBtn = document.querySelector(".btn-open");
 const closeModalBtn = document.querySelector(".btn-close");
 const closeModalOverlay = document.querySelector(".overlay");
-const submitBtn = document.querySelector('.btn submit');
-const booksGrid = document.querySelector('.books-grid');
 
 
-const plusBookBtn = function () {
+const plusBookBtn = () => {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 }
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 }
 
+// Open Input Form
 openModalBtn.addEventListener("click", plusBookBtn);
 closeModalBtn.addEventListener("click", closeModal);
 closeModalOverlay.addEventListener("click", closeModal);
 closeModalOverlay.addEventListener("click", closeModal);
-
-//Add Book Card to library
-
-const createBookInput = () => {
-  resetBookInput()
-  for (let book of Library.books) {
-    addBookCard(book)
-  }
-}
-
-// Adding the HTML (Visual) of the New Book to Library
-
-const addBookCard = (book) => {
-  const bookCard = document.createElement('div')
-  const title = document.createElement('p')
-  const author =  document.createElement('p')
-  const pages = document.createElement('p')
-  const buttonDiv = document.createElement('div')
-  const haveReadBtn = document.createElement('button')
-  const removeBookBtn = document.createElement('button')
-
-
-  bookCard.classList.add('book-card')
-  buttonDiv.classList.add('button-div')
-  haveReadBtn.onclick = toggleRead
-  removeBookBtn.onclick = removeBook
-
-  title.textContent = `"${book.title}"`
-  author.textContent = book.author
-  pages.textContent = `${book.pages} pages`
-  removeBookBtn.textContent = 'Remove'
-
-// Appending to HTML
-
-  bookCard.append(title)
-  bookCard.append(author)
-  bookCard.append(pages)
-  buttonDiv.append(haveReadBtn)
-  buttonDiv.append(removeBookBtn)
-}
-
-const getBookFromInput = () => {
-  const title = document.getElementById('title').value
-  const author = document.getElementById('author').value
-  const pages = document.getElementById('pages').value
-  const isRead = document.getElementById('isRead').checked
-  return new Book(title, author, pages, isRead)
-}
-
-const addBook = (e) => {
-  e.preventDefault()
-  const newBook = getBookFromInput()
-}
 
 
 // Animations
 function animateInput() {
   document.getElementById('btn btn-open').className = 'classname';
 }
+// Book Prototype
+class Book {
+  constructor(
+    title = '',
+    author = '',
+    pages = '',
+    isRead = false
+  ) {
+    this.title =  title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+  }
+}
+// Book Library (Array)
+class Library {
+  constructor() {
+    this.archives = [];
+  }
+
+addBook(book) {
+  if (!this.isInArchives(book)) {
+  archives.push(book);
+  }
+}
+  isInArchives(book) {
+    return this.archives.some((book) => book.title === book.title)
+  }
+}
+  const library = new Library
+  document.forms[0].reset();
+
+
+// Local Saving & Submit button (To log into library)
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.getElementById('addBookBtn').addEventListener('click', Library);
+})
--- Library Basic/test.js
+++ Library Basic/test.js
@@ -1,66 +1,15 @@
-const parent = document.getElementById ('parent').lastElementChild
-console.log(parent)
+const parent = document.getElementById('dingding');
+parent.style.width = '150px';
+parent.style.height = '150px';
+parent.style.backgroundColor = 'red';
 
-const parent2 = document.getElementById('parent').children[3]
-console.log(parent2)
+document.getElementById('newThing').addEventListener("click", newThing);
 
-const secondchild = document.getElementById('secondchild')
-console.log(secondchild)
-
-console.log(secondchild.parentNode)
-
-console.log(secondchild.nextElementSibling)
-
-console.log(secondchild.previousElementSibling)
-
-
-// Create the Element, add CONTENT to the Element, Show the element in DOM.
-/*
-const createEl = document.createElement('div')
-console.log(createEl)
-
-const innerhtml = createEl.innerHTML = 'i am a frontend developer'
-
-// Now make it APPEAR
-
-const parentE1 = document.getElementById('parent')
-
-parentE1.appendChild(createEl)
-
-console.log(parentEl)*/
-
-// This lets you select where to put the DIV instead of at the last spot by Default.
-
-const parentEl = document.getElementById('parent')
-const firstchildEl = document.getElementById('firstchild')
-
-const createEl = document.createElement('div')
-
-const innerhtml = createEl.innerHTML = 'i am a frontend developer'
-
-parentEl.insertBefore(createEl, firstchildEl)
-  console.log(parentEl)
-
-  // REPLACE a CHILD ELEMENT
-  // document.replaceChild(newNode, existingNode)
-
-/* parentEl.replaceChild(createEl, firstchildEl)
-console.log(parentEl) */
-
-// REMOVE CHILD
-// document.removeChild(element)
-
-parentEl.removeChild(firstchildEl)
-console.log(parentE1)
-
-
-
-// STYLE IN JS
-
-
-const buttonEl = document.getElementById('master')
-buttonEl.addEventListener('click', addFunction)
-
-    function addFunction(){
-        buttonEl.classList.add('button')
-    }
+function newThing() {
+  var newDiv = document.createElement('div');
+  newDiv.style.width = '50px';
+  newDiv.style.height = '50px';
+  newDiv.textContent = 'Entry';
+  newDiv.style.backgroundColor = 'white';
+  parent.appendChild(newDiv);
+}
