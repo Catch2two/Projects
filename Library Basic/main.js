abf436d99f2671cfb41bfa8741c656fe249018f5
Josh <118862870+Catch2two@users.noreply.github.com>
2023-04-10 12:15:14 -0400

    A small fix to CSS

--- Library Basic/main.js
+++ Library Basic/main.js
@@ -12,16 +12,26 @@ class Book {
     this.isRead = isRead
   }
 }
+
+// Storage (The Library that stores the books)
 class Library {
   constructor() {
-    this.books = []
+    this.books = []  // Create Array of Added Books
   }
 
   addLibrary(newBook) {
-    this.books.push(newBook)
+    if (!this.isInLibrary(newBook)) {
+      this.books.push(newBook)
+    }
+  }
+
+  removeBook(title) {
+    this.books = this.books.filter((book) => book.title !== title)
   }
 }
-// CREATE BOOK CARD
+
+const library = new Library()
+
 
 
 
@@ -29,9 +39,10 @@ class Library {
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close")
-const closeModalOverlay = document.querySelector(".overlay")
-const submitBtn = document.querySelector('.btn submit')
+const closeModalBtn = document.querySelector(".btn-close");
+const closeModalOverlay = document.querySelector(".overlay");
+const submitBtn = document.querySelector('.btn submit');
+const booksGrid = document.querySelector('.books-grid');
 
 
 const plusBookBtn = function () {
@@ -65,9 +76,43 @@ const addBookCard = (book) => {
   const title = document.createElement('p')
   const author =  document.createElement('p')
   const pages = document.createElement('p')
+  const buttonDiv = document.createElement('div')
+  const haveReadBtn = document.createElement('button')
+  const removeBookBtn = document.createElement('button')
 
-  bookCard.classList.add('')
+
+  bookCard.classList.add('book-card')
+  buttonDiv.classList.add('button-div')
+  haveReadBtn.onclick = toggleRead
+  removeBookBtn.onclick = removeBook
+
+  title.textContent = `"${book.title}"`
+  author.textContent = book.author
+  pages.textContent = `${book.pages} pages`
+  removeBookBtn.textContent = 'Remove'
+
+// Appending to HTML
+
+  bookCard.append(title)
+  bookCard.append(author)
+  bookCard.append(pages)
+  buttonDiv.append(haveReadBtn)
+  buttonDiv.append(removeBookBtn)
 }
+
+const getBookFromInput = () => {
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const pages = document.getElementById('pages').value
+  const isRead = document.getElementById('isRead').checked
+  return new Book(title, author, pages, isRead)
+}
+
+const addBook = (e) => {
+  e.preventDefault()
+  const newBook = getBookFromInput()
+}
+
 
 // Animations
 function animateInput() {
