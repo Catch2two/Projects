0fe56865810adf5fba921ebce666ab4d7f3080c8
Josh <118862870+Catch2two@users.noreply.github.com>
2023-04-20 18:42:15 -0400

    Finally fixed the Hover! Hover:after/before Confusing Hover O fixed

--- TicTacToe/Script/main.js
+++ TicTacToe/Script/main.js
@@ -1,13 +1,37 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 const cellElements = document.querySelectorAll('[data-cell]')
 const gameBoard = document.getElementById('gameBoard')
 let circleTurn
+
+// Start Game
+initializeGame()
+
+restartButton.addEventListener('click', initializeGame)
+
+function initializeGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+}
 
 cellElements.forEach(cell => {
   cell.addEventListener('click', handleClick, { once: true }) // Records click ONCE
 })
 
+// Check if winner/draw/swap turns if game isnt over
 function handleClick(e) {
   console.log('clicked')
   const cell = e.target
@@ -20,26 +44,26 @@ function handleClick(e) {
   setBoardHoverClass()
 }
 
-function placeMark(cell, currentClass) {
+function placeMark(cell, currentClass) {  // Places a Marker in Box
   cell.classList.add(currentClass)
 }
 function swapTurns() {    // Rotates X to O
   circleTurn = !circleTurn
 }
-function setBoardHoverClass() {
-
+function setBoardHoverClass() {     // Hover Marker before Placing
+  gameBoard.classList.remove(X_CLASS)
+  gameBoard.classList.remove(CIRCLE_CLASS)
+  if (circleTurn){
+    gameBoard.classList.add(CIRCLE_CLASS)
+  } else {
+    gameBoard.classList.add(X_CLASS)
+  }
 }
 
 
 
 
 // IIFE - Immediately Invoked Function Expression
-
-// Game Display and Logic Module
-
-const initializeGame = (() => {
-
-})();
 
 // Player Factory
 const Player = (player, marker) => { 
