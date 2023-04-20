6130ca9119d6a4383e2ff5fac767dfb87a45cfb9
Josh <118862870+Catch2two@users.noreply.github.com>
2023-04-20 17:43:12 -0400

    + CSS for Markers + New Names for Divs + handleClick on Gameboard + PlaceMarker function

--- TicTacToe/Script/main.js
+++ TicTacToe/Script/main.js
@@ -1,39 +1,65 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const cellElements = document.querySelectorAll('[data-cell]')
+let circleTurn
+
+cellElements.forEach(cell => {
+  cell.addEventListener('click', handleClick, { once: true }) // Records click ONCE
+})
+
+function handleClick(e) {
+  console.log('clicked')
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  // Check if Win
+  // Check if Draw
+  // switch turns
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+
+
+const startGameButton = document.querySelector('.startGame')
+const gameContainer = document.querySelector('.gameContainer')
+const resetGameButton = document.querySelector('.restartButton')
+const scoreBoard = document.querySelector('.scoreBoard')
+const roundNumber = document.querySelector('.roundInfo')
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],      
+  [0, 3, 6],      
+  [1, 4, 7],      
+  [2, 5, 8],      
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+
+
 // Tic Tac Toe Gameboard Module
 const Gameboard = (() => {
-  const gameBoard = ['', '', '', '', '', '', '', '', '']; // Board Array
-  const turnCount = 0;
+  const gameboardArray = ['', '', '', '', '', '', '', '', '']; // Board Array
+  const getGameboard = () => {
+    console.log(gameboardArray);
+    return gameboardArray;
+
+  }
 
   const reset = () => {
     gameBoard.board = ['', '', '', '', '', '', '', '', '']; // Reset Board Array
 }
+
+  return { getGameboard, reset }
 })(); // IIFE - Immediately Invoked Function Expression
 
 // Game Display and Logic Module
-const newGame = (() => {
-  const startGameButton = document.querySelector('.startGame')
-        gameContainer = document.querySelector('.gameContainer')
-        resetGameButton = document.querySelector('.restartButton')
-        gameBoardElements = document.querySelector('.gameBoard')
-        scoreBoard = document.querySelector('.scoreBoard')
-        newRound = document.querySelector('.roundInfo')
-        winConditions = [
-          [0, 1, 2],
 
-          [3, 4, 5],
-      
-          [6, 7, 8],
-      
-          [0, 3, 6],
-      
-          [1, 4, 7],
-      
-          [2, 5, 8],
-      
-          [0, 4, 8],
-      
-          [2, 4, 6]
-      
-        ];
+const initializeGame = (() => {
 
 })();
 
@@ -45,7 +71,8 @@ const Player = (player, marker) => {
 
 const playerOne = Player('Josh', 'X');  // Create Player
 const playerTwo = Player('Meaghan', 'O');
+const ai = Player('Ai', 'O');
 
-playerOne.welcome();
-playerTwo.welcome();
+playerOne.welcome()
+playerTwo.welcome()
 
