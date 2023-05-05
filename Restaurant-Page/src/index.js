e3243d4a89f5952849a0c69a8956896e67a45d1f
Josh <118862870+Catch2two@users.noreply.github.com>
2023-05-05 14:12:10 -0400

    Adding to the DOM through JS. index.js source --Send--> main.js.

--- Restaurant-Page/src/index.js
+++ Restaurant-Page/src/index.js
@@ -1,3 +1,29 @@
+window.onload = () => {
+    const content = document.querySelector("div#content");
+    // Header
+    const header = document.createElement("div");
+    header.classList.add("header");
+    header.textContent = "Header";
+    content.appendChild(header);
+    // About
+    const about = document.createElement("div");
+    about.classList.add("about");
+    content.appendChild(about);
+    // About Img
+    const img = document.createElement("img");
+    img.src = "/src/img/food.png";
+    about.appendChild(img);
+    // About Info
+    const aboutInfo = document.createElement("p");
+    aboutInfo.classList.add("aboutInfo");
+    aboutInfo.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scio enim esse quosdam, qui quavis lingua philosophari"
+    about.appendChild(aboutInfo);
+    // Menu
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+    content.appendChild(menu);
 
+  };
 
 console.log('HELLO')
+console.log('JOSH')
---
+++ Restaurant-Page/src/menu.js
@@ -0,0 +1,46 @@
+/*<div class="header">Mocky Oglo Jr's</div>
+    <div id="about">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scio enim esse quosdam, qui quavis lingua philosophari possint; Ab his oratores, ab his imperatores ac rerum publicarum principes extiterunt. Quis autem honesta in familia institutus et educatus ingenue non ipsa turpitudine, etiamsi eum laesura non sit, offenditur? Sed audiamus ipsum: Compensabatur, inquit, tamen cum his omnibus animi laetitia, quam capiebam memoria rationum inventorumque nostrorum. Quod quidem iam fit etiam in Academia. Hoc est non dividere, sed frangere. Sedulo, inquam, faciam. Serpere anguiculos, nare anaticulas, evolare merulas, cornibus uti videmus boves, nepas aculeis. Duo Reges: constructio interrete. </p>
+        <img src="/src/img/food.png">
+    </div>
+    <div class="menu">
+        <h1>Menu</h1>
+        <div class="columns">
+            <div class="left">
+                <li>non ipsa turpitudine, etiamsi eum laesura non sit, offenditur. 66$</li>
+                <span class="foodImg">
+                    <img src="/src/img/food2.png">
+                </span>
+                <li>non ipsa turpitudine, etiamsi eum laesura non sit, offenditur. 66$</li>
+                <span class="foodImg">
+                    <img src="/src/img/food2.png">
+                </span>
+                <li>non ipsa turpitudine, etiamsi eum laesura non sit, offenditur. 66$</li>
+                <span class="foodImg">
+                    <img src="/src/img/food2.png">
+                </span>
+                <li>non ipsa turpitudine, etiamsi eum laesura non sit, offenditur. 66$</li>
+                <span class="foodImg">
+                    <img src="/src/img/food2.png">
+                </span>
+            </div>
+            <div class="right">
+                <li>Quod quidem iam fit etiam in Academia. 20$.</li>
+                <span class="foodImg">
+                    <img src="/src/img/food2.png">
+                </span>
+                <li>Quod quidem iam fit etiam in Academia. 20$.</li>
+                <span class="foodImg">
+                    <img src="/src/img/food2.png">
+                </span>
+                <li>Quod quidem iam fit etiam in Academia. 20$.</li>
+                <span class="foodImg">
+                    <img src="/src/img/food2.png">
+                </span>
+                <li>Quod quidem iam fit etiam in Academia. 20$.</li>
+                <span class="foodImg">
+                    <img src="/src/img/food2.png">
+                </span>
+            </div>
+        </div>
+    </div> */ 
