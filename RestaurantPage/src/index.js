fc26223d1621dd1d24ae6900649036f0ec9fefb0
Josh <118862870+Catch2two@users.noreply.github.com>
2023-05-09 11:24:05 -0400

    Added nav Buttons to the Banner

--- RestaurantPage/src/index.js
+++ RestaurantPage/src/index.js
@@ -3,12 +3,26 @@ import banner from "./pages/banner";
 import home from "./pages/home";
 import menu from "./pages/menu";
 import about from "./pages/about";
+init()
+
+// Banner Buttons
+function navButtons(){
+    const homeBtn = document.getElementsByClassName('homeButton') 
+
+    homeBtn.addEventListener('click', home)
+}
+
+function init() {
+    banner()
+    
+}
 
 // Test
+/*
 banner()
 about()
 menu()
-home()
+home()*/
 console.log("Hi")
 console.log("josh")
 
--- RestaurantPage/src/pages/banner.js
+++ RestaurantPage/src/pages/banner.js
@@ -10,6 +10,28 @@ const banner = function(){
     bannerTitle.classList.add("bannerTitle");
     bannerTitle.innerHTML = "<h1>Marv McRubaton Nightly Meat Market</h1>"
     banner.appendChild(bannerTitle);
+
+// Buttons
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttonContainer");
+    content.appendChild(buttonContainer);
+
+    const homeButton = document.createElement("button");
+    homeButton.classList.add("homeButton");
+    homeButton.innerText = "Home";
+    buttonContainer.appendChild(homeButton);
+
+    
+    const aboutButton = document.createElement("button");
+    aboutButton.classList.add("aboutButton");
+    aboutButton.innerText = "About Us";
+    buttonContainer.appendChild(aboutButton);
+
+    
+    const menuButton = document.createElement("button");
+    menuButton.classList.add("menuButton");
+    menuButton.innerText = "Menu";
+    buttonContainer.appendChild(menuButton);
 }
 
 export default banner;
