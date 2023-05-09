682f33787b59f76dc20ca7f373f1ec2a67f6ecea
Josh <118862870+Catch2two@users.noreply.github.com>
2023-05-09 12:28:59 -0400

    made the index the first page load combined the banner.js into index.js

--- RestaurantPage/src/index.js
+++ RestaurantPage/src/index.js
@@ -1,28 +1,61 @@
 // Pages
-import banner from "./pages/banner";
 import home from "./pages/home";
 import menu from "./pages/menu";
 import about from "./pages/about";
-init()
 
 // Banner Buttons
-function navButtons(){
-    const homeBtn = document.getElementsByClassName('homeButton') 
+const banner = function(){
+    const content = document.querySelector("#content");
 
-    homeBtn.addEventListener('click', home)
+    const banner = document.createElement("div")
+    banner.classList.add("banner")
+    content.appendChild(banner)
+    console.log("Banner")
+
+    const bannerTitle = document.createElement("p");
+    bannerTitle.classList.add("bannerTitle");
+    bannerTitle.innerHTML = "<h1>Marv McRubaton Nightly Meat Market</h1>"
+    banner.appendChild(bannerTitle);
+}
+// Buttons
+function buttons() {
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttonContainer");
+    content.appendChild(buttonContainer);
+
+    const homeButton = document.createElement("button");
+    homeButton.classList.add("homeButton");
+    homeButton.innerText = "Home";
+    buttonContainer.appendChild(homeButton);
+
+    
+    const aboutButton = document.createElement("button");
+    aboutButton.classList.add("aboutButton");
+    aboutButton.innerText = "About Us";
+    buttonContainer.appendChild(aboutButton);
+
+    
+    const menuButton = document.createElement("button");
+    menuButton.classList.add("menuButton");
+    menuButton.innerText = "Menu";
+    buttonContainer.appendChild(menuButton);
 }
 
 function init() {
     banner()
-    
+    buttons()
 }
+
+// Always load Banner on every page
+document.addEventListener("DOMContentLoaded", init);
+
 
 // Test
 /*
-banner()
 about()
 menu()
-home()*/
+home()
 console.log("Hi")
 console.log("josh")
 
+*/
