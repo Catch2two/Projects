06c6b1882275b474da3c27e1761776a3013d3201
Josh <118862870+Catch2two@users.noreply.github.com>
2023-05-18 19:00:02 -0400

    clean code

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,5 +2,4 @@ import UI from './modules/UI';
 import theList from './modules/theList'
 import buttons from './modules/buttons'
 buttons()
-theList()
 UI();
--- todo/src/modules/buttons.js
+++ todo/src/modules/buttons.js
@@ -1,14 +1,24 @@
+import UI from './UI';
+
 export default function() {
+    const content = document.querySelector('#content');
+
     const button1 = document.createElement('button');
-    button1.textContent = 'Add Button';
+    button1.textContent = 'Important Tasks';
     button1.addEventListener('click', function() {
         console.log('Button One Clicked')
     });
     const button2 = document.createElement('button');
-    button2.textContent = 'Add Button';
+    button2.textContent = 'Weekly Tasks';
     button2.addEventListener('click', function() {
         console.log('Button Two Clicked')
     });
+    const button3 = document.createElement('button');
+    button3.textContent = 'Daily Tasks';
+    button3.addEventListener('click', function() {
+        console.log('Button Three Clicked')
+    })
     document.querySelector('#content').appendChild(button1);
     document.querySelector('#content').appendChild(button2);
+    document.querySelector('#content').appendChild(button3);
 }
