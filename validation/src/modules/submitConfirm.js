4cea7a77de573ee8deb62f3aa7170e5398e193f7
Josh <118862870+Catch2two@users.noreply.github.com>
2023-06-22 12:45:30 -0400

    Rehauled a cleaner JS

--- validation/src/modules/submitConfirm.js
+++ validation/src/modules/submitConfirm.js
@@ -2,113 +2,64 @@ const email = document.getElementById("email");
 const country = document.getElementById("country");
 const zipCode = document.getElementById("zipCode");
 const password = document.getElementById("password");
-const passwordC = document.getElementById("passwordConfirm");
+const passwordConfirm = document.getElementById("passwordConfirm");
 const submitButton = document.getElementById("submitBtn");
+
 // Errors
-const errorE = document.querySelector(".errorE");
-const errorC = document.querySelector(".errorC");
-const errorZ = document.querySelector(".errorZ");
-const errorPW = document.querySelector(".errorPW");
-const errorPWC = document.querySelector(".errorPWC");
+const emailError = document.querySelector(".errorE");
+const countryError = document.querySelector(".errorC");
+const zipCodeError = document.querySelector(".errorZ");
+const passwordError = document.querySelector(".errorPW");
+const passwordConfirmError = document.querySelector(".errorPWC");
 
 // Events
 
+function showError(errorElement, message) {
+  errorElement.textContent = message;
+}
+
 email.addEventListener("input", (event) => {
-    if (email.validity.valid) {
-        errorE.textContent = ""
-    } else {
-        showErrorE();
-        event.preventDefault();
-    }
+  if (email.validity.valid) {
+    showError(emailError, "");
+  } else {
+    showError(emailError, "Please enter an email address.");
+  }
 });
 
 country.addEventListener("input", (event) => {
-    if (country.validity.valid) {
-        errorC.textContent = "";
-    } else {
-        showErrorC();
-        event.preventDefault();
-    }
+  if (country.validity.valid) {
+    showError(countryError, "");
+  } else {
+    showError(countryError, "Please enter a country.");
+  }
 });
 
 zipCode.addEventListener("input", (event) => {
-    if (zipCode.validity.valid) {
-        errorZ.textContent = "";
-    } else {
-        showErrorZ();
-        event.preventDefault();
-    }
+  if (zipCode.validity.valid) {
+    showError(zipCodeError, "");
+  } else {
+    showError(zipCodeError, "Please enter a valid ZIP code.");
+  }
 });
 
 password.addEventListener("input", (event) => {
-    if (password.validity.valid) {
-        errorPW.textContent = "";
-    } else {
-        showErrorPW();
-        event.preventDefault();
-    }
+  if (password.validity.valid) {
+    showError(passwordError, "");
+  } else {
+    showError(passwordError, "Please enter a password.");
+  }
 });
 
-passwordC.addEventListener("input", (event) => {
-    if (passwordC.validity.valid) {
-        errorPWC.textContent = "";
-    } else {
-        showErrorPWC();
-        event.preventDefault();
-    }
+passwordConfirm.addEventListener("input", (event) => {
+  if (passwordConfirm.value === password.value) {
+    showError(passwordConfirmError, "");
+  } else {
+    showError(passwordConfirmError, "Passwords do not match.");
+  }
 });
 
-passwordC.addEventListener("input", (event) => {
-    showErrorPWC();
+submitButton.addEventListener("click", (event) => {
+  if (passwordConfirm.value !== password.value) {
+    event.preventDefault();
+  }
 });
-// ERROR MESSAGES
-function showErrorE() {
-    if (email.validity.valueMissing) {
-        errorE.textContent = "Please enter an Email Address."
-    } else if (email.validity.typeMismatch) {
-        errorE.textContent = "Entered value needs to be an email address."
-    } else if (email.validity.tooShort) {
-        errorE.textContent = `Email should be atleast ${email.minLength} characters; you entered ${email.value.length}.`;
-    } else if (!email.value.indexOf('@') > -1) {
-        errorE.textContent = "Please enter an Email Address"
-    } else if (email.value.indexOf('@') > -1) {
-        errorE.textContent = ""
-    } 
-};
-
-function showErrorC() {
-    if (country.validity.valueMissing) {
-        errorC.textContent = "Please enter a country.";
-    } else if (country.validity.tooShort) {
-        errorC.textContent = "Country must be atleast 3 characters."
-    }
-};
-function showErrorZ() {
-    if (zipCode.value.length < 5) {
-        errorZ.textContent = "Please enter a valid ZIP Code.";
-    }
-
-};
-function showErrorPW() {
-    if (password.validity.valueMissing) {
-        errorPW.textContent = "Please enter a new password.";
-    } else if (password.validity.tooShort) {
-        errorPW.textContent = `Invalid password. Password must be at least 8 characters long.`;
-    } 
-
-};
-function showErrorPWC() {
-    if (passwordC.value !== password.value) {
-        errorPWC.textContent = "Passwords do not match.";
-        submitButton.disabled = true;
-    } else {
-        submitButton.disabled = false;
-    }
-    
-    submitButton.addEventListener("click", (event) => {
-        if (submitButton.disabled) {
-            event.preventDefault();
-        }
-    });
-}
-
