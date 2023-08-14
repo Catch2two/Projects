a3a1285d80237870195092d163baca4f61ffdf93
Josh <118862870+Catch2two@users.noreply.github.com>
2023-08-14 14:59:39 -0400

    GetLastNode getFirstNode

--- Linked List/ClassLinkedList.js
+++ Linked List/ClassLinkedList.js
@@ -10,7 +10,20 @@ class LinkedList {
         this.head = head;
         this.size = 0;
     }
-
+getFirstNode() {
+    if (this.head !== null) {
+        return this.head;
+    } else {
+        return null;
+    }
+};
+getLastNode() {
+    let currentNode = this.head;
+    while (currentNode.next) {
+        currentNode = currentNode.next
+    }
+    return currentNode
+}
 addNodeToEnd(data) {
     let newNode = new ListNode(data);
 
@@ -33,9 +46,31 @@ addNodeToEnd(data) {
     return this;
 };
 
+removeNodeFromEnd() {
+    let currentNode = this.head;
+    let previousNode = null;
+
+    while (currentNode.next) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+    }
+
+    if (previousNode !== null) {
+        previousNode.next = null;
+    } else {
+        this.head = null;
+    }
+
+    this.size--;
+}
+
 clear() {
     this.head = null;
     this.size = 0;
+};
+
+getSize() {
+    return this.size;
 }
 
 printList() {
@@ -61,12 +96,21 @@ let list2 = new LinkedList();
 for (let number of numbers) {
     list2.addNodeToEnd(number);
 }
-
 // list2.printList() // 100 89 9 78
 
-
+// This list
 console.log("The list before clearing:");
 list.printList();
+// The list size
+console.log("the size of the list is:", list.getSize()) // the size of the list is: 12
+// Select first node
+console.log("The first node is:", list.getFirstNode().data)
+// Select last node
+console.log("The last node is:", list.getLastNode().data)
+// Remove last object in list
+list.removeNodeFromEnd();
+list.printList();
+// Clear list
 list.clear();
 console.log("The list after clearing:");
-list.printList();
+list.printList(); // The list after clearing:
