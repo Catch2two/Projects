a6d7c8201c8468e5cc36bcc71d37bc22c3a51a0a
Josh <118862870+Catch2two@users.noreply.github.com>
2023-08-22 09:23:43 -0400

    Setup example from FreeCodeCamp YT video

---
+++ BinarySearchTree/BST.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data
+        this.left = left
+        this.right = right
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    add(data) {
+        const node = this.root;
+        if(node === null) {
+            this.root = new Node(data);
+            return;
+        } else {
+            const searchTree = function(node) {
+                if(data < node.data) {
+                    if(node.left === null) {
+                        node.left = new Node(data);
+                        return;
+                    } else if (node.left !== null) {
+                        return searchTree(node.left)
+                    } else if (node.right !== null) {
+                        return searchTree(node.right)
+                    }
+                }
+            }
+        }
+    }
+}
