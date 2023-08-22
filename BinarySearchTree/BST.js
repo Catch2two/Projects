75eaa7970d391157ac08c3e1e31cd78f22b09e2c
Josh <118862870+Catch2two@users.noreply.github.com>
2023-08-22 11:12:39 -0400

    Added Console commands to test

--- BinarySearchTree/BST.js
+++ BinarySearchTree/BST.js
@@ -114,3 +114,19 @@ class BST {
         this.root = removeNode(this.root, data);
     }
 }
+
+const bst = new BST();
+
+bst.add(4);
+bst.add(2);
+bst.add(6);
+bst.add(1);
+bst.add(3);
+bst.add(5);
+bst.add(7);
+bst.remove(4)
+console.log(bst.findMin());
+console.log(bst.findMax());
+bst.remove(7)
+console.log(bst.findMax());
+console.log(bst.isPresent(4))
