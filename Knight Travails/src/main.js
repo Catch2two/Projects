72a5b753262098c59bd3b9e0c235465d7608d000
Josh <118862870+Catch2two@users.noreply.github.com>
2023-08-31 15:36:27 -0400

    Started with Creating the 8x8 Board.

---
+++ Knight Travails/src/main.js
@@ -0,0 +1,11 @@
+function createGameboard() {
+    // Create a 8x8 gameboard.
+    let gameboard = new Array(8);
+    for (let i = 0; i < 8; i++) {
+        gameboard[i] = new Array(8);
+        for (let j = 0; j < 8; j++) {
+            gameboard[i][j] = 0  // Start each cell with 0
+        }
+    }
+    return gameboard;
+}
