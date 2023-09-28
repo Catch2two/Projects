1bb4853a6405b0f1c5d8bcdc2893fcb330799578
Josh <118862870+Catch2two@users.noreply.github.com>
2023-09-28 13:31:47 -0400

    Test Specific coordination for Gameboard

--- Battleship/src/components/gameboard.js
+++ Battleship/src/components/gameboard.js
@@ -1,12 +1,49 @@
 const Ship = require('../components/ship');
+
 class Gameboard {
-  constructor() {
-    this.boardArray = Array.from({ length: 10 }, () =>
-      Array.from({ length: 10 }, () => 0)
-    );
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.board = new Array(this.height).fill(new Array(this.width).fill(null));
   }
-  printGameBoard() {
-    console.table(this.boardArray);
+
+  placeShip(ship, x, y, orientation) {
+    if (orientation === 'horizontal') {
+      for (let i = 0; i < ship.size; i++) {
+        if (x + i >= this.width || this.board[y][x + i] !== null) {
+          return false;
+        }
+      }
+
+      for (let i = 0; i < ship.size; i++) {
+        this.board[y][x + i] = ship;
+      }
+    } else if (orientation === 'vertical') {
+      for (let i = 0; i < ship.size; i++) {
+        if (y + i >= this.height || this.board[y + i][x] !== null) {
+          return false;
+        }
+      }
+
+      for (let i = 0; i < ship.size; i++) {
+        this.board[y + i][x] = ship;
+      }
+    } else {
+      throw new Error('Invalid orientation: Must be Vertical or Horizontal');
+    }
+    return true;
   }
 }
+
+const gameboard = new Gameboard(10, 10);
+
+// Create a new ship object of size 4
+const ship = new Ship('Battleship', 4);
+
+// Place the ship at coordinates (0, 0) in a horizontal orientation
+gameboard.placeShip(ship, 0, 0, 'horizontal');
+
+// Check the value of the gameboard at coordinates (0, 0)
+console.log(gameboard.board[0][0]); // Outputs: Ship { name: 'Battleship', size: 4 }
+
 module.exports = Gameboard;
--- Battleship/src/tests/gameboard.test.js
+++ Battleship/src/tests/gameboard.test.js
@@ -1,4 +1,14 @@
 const Gameboard = require('../components/gameboard');
 const Ship = require('../components/ship');
 
-test('gameboard initiation', () => {});
+test('Specific location test', () => {
+  const gameboard = new Gameboard(10, 10);
+  gameboard.placeShip(new Ship('Battleship', 4), 0, 0, 'horizontal');
+  expect(gameboard.board[1][2]).toEqual({
+    name: 'Battleship',
+    size: 4,
+    hit: false,
+    hits: 0,
+    isSunk: false,
+  });
+});
