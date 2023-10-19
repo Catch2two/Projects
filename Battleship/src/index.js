143947d09a5db0900d50919a021452eeeae4fd0e
Josh <118862870+Catch2two@users.noreply.github.com>
2023-10-19 13:00:02 -0400

    More work on webpack reinstall

--- Battleship/src/index.js
+++ Battleship/src/index.js
@@ -4,6 +4,4 @@ const ship = require('ship');
 const gameboard = require('gameboard');
 const gameplay = require('gameplay');
 
-const player1 = new Player();
-const game = new gameboard();
 console.log('Start Here!!');
