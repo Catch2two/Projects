e8a90fc2e8439076b67d8aba1f7e7c992fc4dbc6
Josh <118862870+Catch2two@users.noreply.github.com>
2024-02-14 09:57:10 -0500

    attempt 2, I keep forgetting everything...

---
+++ cvproject/cvproject/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>First JSX Project.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
