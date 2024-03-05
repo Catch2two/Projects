5aff39994a28511cf7da95d4e1dd9c8922f22fdc
Josh <118862870+Catch2two@users.noreply.github.com>
2024-03-05 12:52:56 -0500

    combined schools into one JSX

--- cvproject/cvproject/CVproject/src/main.jsx
+++ cvproject/cvproject/CVproject/src/main.jsx
@@ -3,16 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import CvInfo from "./info.jsx";
 import School from "./school.jsx";
-import School2 from "./school2.jsx";
-import School3 from "./school3.jsx";
 import Experiences from "./experiences.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CvInfo />
     <School />
-    <School2 />
-    <School3 />
     <Experiences />
   </React.StrictMode>
 );
--- cvproject/cvproject/CVproject/src/school.jsx
+++ cvproject/cvproject/CVproject/src/school.jsx
@@ -5,11 +5,31 @@ function School() {
   const titleOfStudy = "Bachelors of Fine Arts / Illustration Major";
   const dateOfStudy = "2013-2015";
 
+  const schoolName2 = "Manchester Community College";
+  const titleOfStudy2 = "Computer Science/Graphic Design";
+  const dateOfStudy2 = "2008-2010";
+
+  const schoolName3 = "Northwestern Connecticut Community College";
+  const titleOfStudy3 = "Graphic Design/Fine Arts";
+  const dateOfStudy3 = "2010-2012";
+
   return (
-    <div className="school">
-      <h3>{schoolName}</h3>
-      <h4>{titleOfStudy}</h4>
-      <h5>{dateOfStudy}</h5>
+    <div className="school-container">
+      <div className="school">
+        <h3>{schoolName}</h3>
+        <h4>{titleOfStudy}</h4>
+        <h5>{dateOfStudy}</h5>
+      </div>
+      <div className="school2">
+        <h3>{schoolName2}</h3>
+        <h4>{titleOfStudy2}</h4>
+        <h5>{dateOfStudy2}</h5>
+      </div>
+      <div className="school3">
+        <h3>{schoolName3}</h3>
+        <h4>{titleOfStudy3}</h4>
+        <h5>{dateOfStudy3}</h5>
+      </div>
     </div>
   );
 }
